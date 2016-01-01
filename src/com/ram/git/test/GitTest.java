a215d758af1fe18d2ab2584021a00de9d72016a0
Ram <Ram@10.0.0.103>
2015-12-31 22:23:14 -0500

    Practice Test

--- src/com/ram/git/test/GitTest.java
+++ src/com/ram/git/test/GitTest.java
@@ -3,7 +3,7 @@ package com.ram.git.test;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Hello Git Plugin  Test");
 	}
 
 }
