6b858298f82d293fe78459dab40b7bc91e973cac
Ram Krish <e@mail.com>
2015-12-31 22:33:58 -0500

    Commiting to new branch bug@1234

--- src/com/ram/git/test/GitTest.java
+++ src/com/ram/git/test/GitTest.java
@@ -3,7 +3,7 @@ package com.ram.git.test;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git Plugin  Test");
+		System.out.println("Hello Git Plugin  Test from branch bug#1234");
 	}
 
 }
